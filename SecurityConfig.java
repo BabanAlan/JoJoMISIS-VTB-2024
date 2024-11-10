45e5c4303ed2341dfa45c431ff3ac294a8b24da2
BabanAlan <99186635+BabanAlan@users.noreply.github.com>
2024-11-10 19:55:00 +0300

    Create SecurityConfig.java
    работа с OAuth2

---
+++ SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeRequests(auth -> auth
+                .antMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+}
